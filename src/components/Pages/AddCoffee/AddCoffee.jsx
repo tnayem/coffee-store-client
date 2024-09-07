c7c299db6fb47bf62f6f4276d59e3a5c9694ec9f
Md Tasdid Nayem <tasdidnayem@gmail.com>
2024-09-08 04:03:24 +0600

    Implement add coffee function

--- src/components/Pages/AddCoffee/AddCoffee.jsx
+++ src/components/Pages/AddCoffee/AddCoffee.jsx
@@ -1,6 +1,34 @@
 import { Link } from 'react-router-dom';
 import './AddCoffee.css'
 const AddCoffee = () => {
+    const addCoffee=(e)=>{
+        e.preventDefault()
+        const form =e.target
+        const coffeeName = form.coffeeName.value 
+        const chefName = form.chefName.value 
+        const coffeeSupplier = form.coffeeSupplier.value 
+        const coffeeTaste = form.coffeeTaste.value 
+        const coffeeCategory = form.coffeeCategory.value 
+        const coffeeDetails = form.coffeeDetails.value 
+        const photoURL = form.photoURL.value 
+        const newCoffee = {
+            coffeeName,
+            chefName,
+            coffeeSupplier,
+            coffeeTaste,
+            coffeeCategory,
+            coffeeDetails,
+            photoURL
+
+        }
+        console.log(newCoffee);
+
+
+
+
+
+
+    }
     return (
         <div id="add-coffee" className="container mx-auto py-10">
             <Link to='/' className='btn mx-36 px-5 rounded-none my-5'>Back to Home</Link>
@@ -8,50 +36,50 @@ const AddCoffee = () => {
                 <h3 className="text-3xl font-bold text-center">Add New Coffee</h3>
                 <p className="text-center font-semibold"><small>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at <br /> its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed <br /> to using Content here.</small></p>
                 <div>
-                    <form>
+                    <form onSubmit={addCoffee}>
                         <div className="grid lg:grid-cols-2 gap-6 lg:px-40">
                             <div>
                                 <div className="label">
                                     <span className="label-text">Name</span>
                                 </div>
-                                <input type="text" placeholder="Enter Coffee Name" className="input input-bordered w-full" />
+                                <input type="text" name='coffeeName' placeholder="Enter Coffee Name" className="input input-bordered w-full" />
                             </div>
                             <div>
                                 <div className="label">
-                                    <span className="label-text">Name</span>
+                                    <span className="label-text">Chef</span>
                                 </div>
-                                <input type="text" placeholder="Enter Coffee Name" className="input input-bordered w-full" />
+                                <input type="text" name='chefName' placeholder="Enter coffee chef" className="input input-bordered w-full" />
                             </div>
                             <div>
                                 <div className="label">
-                                    <span className="label-text">Name</span>
+                                    <span className="label-text">Supplier</span>
                                 </div>
-                                <input type="text" placeholder="Enter Coffee Name" className="input input-bordered w-full" />
+                                <input type="text" name='coffeeSupplier' placeholder="Enter coffee supplier" className="input input-bordered w-full" />
                             </div>
                             <div>
                                 <div className="label">
-                                    <span className="label-text">Name</span>
+                                    <span className="label-text">Taste</span>
                                 </div>
-                                <input type="text" placeholder="Enter Coffee Name" className="input input-bordered w-full" />
+                                <input type="text" name='coffeeTaste' placeholder="Enter coffee taste" className="input input-bordered w-full" />
                             </div>
                             <div>
                                 <div className="label">
-                                    <span className="label-text">Name</span>
+                                    <span className="label-text">Category</span>
                                 </div>
-                                <input type="text" placeholder="Enter Coffee Name" className="input input-bordered w-full" />
+                                <input type="text" name='coffeeCategory' placeholder="Enter coffee category" className="input input-bordered w-full" />
                             </div>
                             <div>
                                 <div className="label">
-                                    <span className="label-text">Name</span>
+                                    <span className="label-text">Details</span>
                                 </div>
-                                <input type="text" placeholder="Enter Coffee Name" className="input input-bordered w-full" />
+                                <input type="text" name='coffeeDetails' placeholder="Enter coffee details" className="input input-bordered w-full" />
                             </div>
                         </div>
                         <div className="lg:px-40">
                             <div className="label">
-                                <span className="label-text">Name</span>
+                                <span className="label-text">Photo</span>
                             </div>
-                            <input type="text" placeholder="Enter Coffee Name" className="input input-bordered w-full" />
+                            <input type="text" name='photoURL' placeholder="Enter photo URL" className="input input-bordered w-full" />
                         </div>
                         <div className='lg:px-40 my-5'>
                             <input className='btn w-full bg-[#D2B48C] rounded-none' type="submit" value="Add Coffee" />
